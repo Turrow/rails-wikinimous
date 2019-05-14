71cdec54c184c6f56c56d39919d31ff613e65659
Leonard Siebold <leo.siebold@hotmail.de>
2019-05-14 17:50:47 +0200

    not finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article.create(title: Faker::Movies::LordOfTheRings.character, content: Faker::Movies::LordOfTheRings.location)
+end
